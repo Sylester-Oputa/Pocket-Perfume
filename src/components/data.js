bff906fc3f57694f039e9c1e91d2dbd4f8bb416d
Sylvester-Oputa <soputa42@gmail.com>
2024-11-13 11:57:26 +0100

    Commit

--- src/components/data.js
+++ src/components/data.js
@@ -20,34 +20,37 @@ const list = [
      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXEMyq_xBQW20iAoZ2sbhLNy7gTtErNJNdCQ&s",
      amount: 1,
    },
+
    {
     id: 4,
+    title: "KHAMRAH LATTAFA",
+    price: 19000,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeOlq87mlVzPf7jkNe73jkaHeEh3rlCOYH0g&s",
+    amount: 1,
+  },
+
+   {
+    id: 5,
     title: "TOM FORD TOBACCO VANILLE EAU DE PARFUM",
     price: 8000,
     img: "https://fragrances.com.ng/media/catalog/product/cache/3f352caa9845cb86b827745a9fdca65c/1/4/148_67288585_1.jpg",
     amount: 1,
   },
   {
-    id: 5,
+    id: 6,
     title: "GENIE COLLECTION PERFUMES",
     price: 16000,
     img: "https://media.zid.store/thumbs/c6174eb7-2065-4fdc-9367-8ba333b53155/f14c0509-b48e-4a5c-abbe-a8f82bdf05ed-thumbnail-1000x1000.png",
     amount: 1,
   },
   {
-    id: 6,
+    id: 7,
     title: "FRAGRANCE WORLD PERFUME BUNDLE",
     price: 23000,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHF4oreCSZJDY66xG9dofIuI9H3ArCcW6msA&s",
     amount: 1,
   },
-  {
-    id: 7,
-    title: "KHAMRAH LATTAFA",
-    price: 19000,
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeOlq87mlVzPf7jkNe73jkaHeEh3rlCOYH0g&s",
-    amount: 1,
-  },
+
   {
     id: 8,
     title: "BERRY WEEKEND EMOTIONS",
