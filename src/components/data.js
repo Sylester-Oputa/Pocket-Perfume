095d866d8b170611d5cad520a259f06c330a8d1a
Sylvester-Oputa <soputa42@gmail.com>
2024-11-13 15:24:24 +0100

    Commit

--- src/components/data.js
+++ src/components/data.js
@@ -28,23 +28,15 @@ const list = [
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeOlq87mlVzPf7jkNe73jkaHeEh3rlCOYH0g&s",
     amount: 1,
   },
-
-   {
-    id: 5,
-    title: "TOM FORD TOBACCO VANILLE EAU DE PARFUM",
-    price: 8000,
-    img: "https://fragrances.com.ng/media/catalog/product/cache/3f352caa9845cb86b827745a9fdca65c/1/4/148_67288585_1.jpg",
-    amount: 1,
-  },
   {
-    id: 6,
+    id: 5,
     title: "GENIE COLLECTION PERFUMES",
     price: 16000,
     img: "https://media.zid.store/thumbs/c6174eb7-2065-4fdc-9367-8ba333b53155/f14c0509-b48e-4a5c-abbe-a8f82bdf05ed-thumbnail-1000x1000.png",
     amount: 1,
   },
   {
-    id: 7,
+    id: 6,
     title: "FRAGRANCE WORLD PERFUME BUNDLE",
     price: 23000,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHF4oreCSZJDY66xG9dofIuI9H3ArCcW6msA&s",
@@ -52,17 +44,25 @@ const list = [
   },
 
   {
-    id: 8,
+    id: 7,
     title: "BERRY WEEKEND EMOTIONS",
     price: 5000,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIpg3PLK6ZRY5gAXrsCzAtSTe_RIuAHyCpXA&s",
     amount: 1,
   },
   {
-    id: 9,
+    id: 8,
     title: "MONOGOTAS VANILLA BUNDLE",
     price: 7000,
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjcIo_c8uz-XCDvclXcflufYg7nAGgMTR0tg&s",
+    amount: 1,
+  },
+
+  {
+    id: 9,
+    title: "TOM FORD TOBACCO VANILLE EAU DE PARFUM",
+    price: 8000,
+    img: "https://fragrances.com.ng/media/catalog/product/cache/3f352caa9845cb86b827745a9fdca65c/1/4/148_67288585_1.jpg",
     amount: 1,
   },
  ];
